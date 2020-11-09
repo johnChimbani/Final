10f7e4d9f0823ebe811e5c0acca2c2644cd52a61
johnchimbani <john.chimbani@pocktor.com>
2020-11-09 20:40:47 +0100

    Jag har tagit bort timeklassen och lagt till en meny med do while loop som gör att man kan välja vad man vill göra.

--- src/se/JohnChimbani/Main.java
+++ src/se/JohnChimbani/Main.java
@@ -1,79 +1,89 @@
 package se.JohnChimbani;
 
-
 import java.util.*;
 
-
-import se.JohnChimbani.time.Time;
 import se.JohnChimbani.user.User;
 
-import javax.swing.*;
+import static javax.swing.JOptionPane.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        int input;
 
-       String[] users = new String[100];
-       users[0] = "user";
+        List<Date> pass = new ArrayList<>();
+        Stack<String> users = new Stack<>();
+        Scanner console = new Scanner(System.in);
 
-        int i=0;
-        //String input = JOptionPane.showInputDialog(null,"Skriv ditt användarnam: ");
-        //if (input.equals(users[0]))  {
+        do {
+            System.out.println(" Välkommen till Brf Klintens Bokningssystem för tvättstugan");
+            System.out.println("\n 1. Lägg till användare");
+            System.out.println("\n 2. Boka pass");
+            System.out.println("\n 3. Visa användare");
+            System.out.println("\n 4. Visa pass ");
+            System.out.println("\n 5. Regler");
+            System.out.println("\n 6. Avsluta ");
+            System.out.println("\n Välj vad du vill göra? ");
+            input = console.nextInt();
+            console.nextLine();
 
-            System.out.println("--------------------------------------------------------------------------------------  Brf Klintens Bokningssystem  -----------------------------------------------------------------------------------------------------");
+            switch (input) {
 
-            System.out.println("\n                                                                                            1. Boka en tid ");
-            System.out.println("\n                                                                                            2. Se bokad tid ");
-            System.out.println("\n                                                                                            3. Skapa användare ");
-            String c = JOptionPane.showInputDialog("Välj vad du vill göra? ");
-
-            switch (c) {
-
-
-                case "1":
-
-                    JOptionPane.showMessageDialog(null,"Välj År/Mån/Dag/Startid");
-                    Calendar yourCalendar = Calendar.getInstance();
-                    String year = JOptionPane.showInputDialog(null, " År(Exempelvis 2021)");
-                    int or = Integer.parseInt(year);
-                    String month = JOptionPane.showInputDialog(null, " Mån(Exempelvis 5 för Maj)");
-                    int monad = Integer.parseInt(month);
-                    monad -=1;
-                    String date = JOptionPane.showInputDialog(null, " Datum(Exempelvis 4)");
-                    int datum = Integer.parseInt(date);
-                    String hour = JOptionPane.showInputDialog(null, " Klockslag(Exempelvis 15 för 15:00)");
-                    int timme = Integer.parseInt(hour);
-
-                   yourCalendar.set(or,monad,datum,timme,0,0);
-                   System.out.println("Din tvätttid är " + yourCalendar.getTime());
-
-                    break;
-                case "2":
-
-                    break;
-                case "3":
-                    int k = 0;
+                case 1:
                     boolean h = true;
-                     String newUser;
-
                     while (true) {
-                        newUser = JOptionPane.showInputDialog(null, "Skriv ett användarnamn");
+                        String newUser = showInputDialog(null, "Skriv ett användarnamn");
                         if (newUser == null)
                             break;
                         User x = new User();
                         x.setCredentials(newUser);
-                        users[i] = newUser;
-                        System.out.println("                                                                                         Du har lagt till : " + x.getCredentials() + " !");
-
-                        }
+                        users.add(newUser);
+                    }
                     break;
 
+                case 2:
 
-        }
+                    String userName = showInputDialog(null, "Användarnamn?");
+                    if (users.search(userName) == 1) {
+                        showMessageDialog(null, "Välj År/Mån/Dag/Startid");
+                        Calendar yourCalendar = Calendar.getInstance();
+                        String year = showInputDialog(null, " År(Exempelvis 2021)");
+                        int or = Integer.parseInt(year);
+                        String month = showInputDialog(null, " Mån(Exempelvis 5 för Maj)");
+                        int monad = Integer.parseInt(month);
+                        monad -= 1;
+                        String date = showInputDialog(null, " Datum(Exempelvis 4)");
+                        int datum = Integer.parseInt(date);
+                        String hour = showInputDialog(null, " Klockslag(Exempelvis 15 för 15:00)");
+                        int timme = Integer.parseInt(hour);
+                        yourCalendar.set(or, monad, datum, timme, 0, 0);
+                        pass.add(yourCalendar.getTime());
+                    } else {
+                        System.out.println("Du är tyväär inte registrerad!");
+                    }
+                    break;
+                case 3:
 
-
-        }
+                    System.out.println("Användare");
+                    for (String u : users)
+                        System.out.println("  " + u);
+                    break;
+                case 4:
+                    System.out.println("Bokade pass");
+                    for (Date p : pass)
+                        System.out.println(" " + p);
+                    break;
+                case 5:
+                    System.out.println("Läs gärna genom våra regler nedan");
+                    System.out.println("\n 1. Tvätttiden gäller i 4 timmar från starttiden");
+                    System.out.println("\n 2. Städa efter dig");
+                    System.out.println("\n 3. Bokade tider försvinner om inte passet startas inom 30 min");
+                    break;
+                case 6:
+                    break;
+            }
+        } while (input != 6);
 
     }
-
+}
--- src/se/JohnChimbani/user/User.java
+++ src/se/JohnChimbani/user/User.java
@@ -1,45 +1,48 @@
 package se.JohnChimbani.user;
 
-
-
 /**
  * Created by John Chimbani
  * Date: 2020-11-06
  * Time: 14:29
  * Project: Inläming_3
  * Copyright: MIT
+ * User class som innehåller
  */
 public class User {
 
-    //Instance variables
-    private  String userName;
-
-
-    //Instance methods
-
-    public String getCredentials()
-    {
-        if (this.userName == null )
-        {
-            this.userName = "Visitor ";
-        }
-
-          return userName;
-    }
-
-
-    public void setCredentials(String user) {
-
-        if (user != null ) {
-            this.userName = user;
-        }
-        else
-
-            throw  new  NullPointerException();    }
+    private String userName;
 
     /**
-     * Method that overrides Java's inbuilt toString method
-     * @return string representation instead of hash code
+     * Metod som är en getter för userName
+     *
+     * @return userName
+     */
+    public String getCredentials() {
+        if (this.userName == null) {
+            this.userName = "Besökare";
+        } else
+            throw new NullPointerException();
+        return userName;
+    }
+
+    /**
+     * Metod som är en setter för userName
+     *
+     * @param user variablen skickas in för att kolla vad användaren har matat in
+     */
+    public void setCredentials(String user) {
+
+        if (user != null) {
+            this.userName = user;
+        } else
+
+            throw new NullPointerException();
+    }
+
+    /**
+     * Metod som kör över Java's inbyggda toString metod
+     *
+     * @return en sträng istället för hashcode
      */
     @Override
     public String toString() {
